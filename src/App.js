185cbe1d3cec7b09bd6c1f3ea0a1ecd3a1ad6c29
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-08-11 08:55:45 +0700

    add spotify auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Login from "./Login";
 import "./App.css";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Kbuhb</h1>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  // Run Code based on [] condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  return <div className="app">{token ? <h1>Main Home</h1> : <Login />}</div>;
 }
 
 export default App;
