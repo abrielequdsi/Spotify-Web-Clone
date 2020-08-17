879825ef5aa30b011948de205b4cf222ae1c5ef3
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-08-17 13:40:53 +0700

    Integrating access token and add Context API

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import "./App.css";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useStateProviderValue } from "./StateProvider";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useStateProviderValue();
 
   // Run Code based on [] condition
   useEffect(() => {
     const hash = getTokenFromUrl();
+    // clear the token from the url (safety)
     window.location.hash = "";
+    // take the actual token from hash object {}
     const _token = hash.access_token;
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
     }
   }, []);
 
-  return <div className="app">{token ? <h1>Main Home</h1> : <Login />}</div>;
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
